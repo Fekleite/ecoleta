99b3888cad90c352a2b5350973c1a86c9ade99a6
Fernanda <fernandacarolineleite360@gmail.com>
2020-06-04 10:50:01 -0300

    Initial files

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,23 +1,17 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+
+import Home from "./src/pages/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-    </View>
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#7159c1",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "#ffffff",
-    fontSize: 20,
-  },
-});
